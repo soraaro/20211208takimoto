d7209b4dc57ed96587e18c01965daf083b971528
sora <sora.0407.kuu@gmail.com>
2021-12-05 19:22:47 +0900

    commmit

--- pages/login.vue
+++ pages/login.vue
@@ -1,19 +1,19 @@
 <template>
-  <div class="register">
-    <p>新規登録</p>
-    <label
-      >メールアドレス： <input v-model="email" type="email" required
-    /></label>
+  <div class="login">
+    <label>
+      メールアドレス： <input v-model="email" type="email" required />
+    </label>
     <br />
-    <label
-      >パスワード： <input v-model="password" type="password" required
-    /></label>
+    <label>
+      パスワード： <input v-model="password" type="password" required />
+    </label>
     <br />
-    <button @click="register">新規登録</button>
+    <button @click="login">ログイン</button>
     <br />
     <NuxtLink to="/">戻る</NuxtLink>
   </div>
 </template>
+
 
 <script>
 import firebase from '~/plugins/firebase'
@@ -25,29 +25,31 @@ export default {
     }
   },
   methods: {
-    register() {
+    login() {
       if (!this.email || !this.password) {
         alert('メールアドレスまたはパスワードが入力されていません。')
         return
       }
       firebase
         .auth()
-        .createUserWithEmailAndPassword(this.email, this.password)
-        .then((data) => {
-          data.user.sendEmailVerification().then(() => {
-            this.$router.replace('/confirm')
-          })
+        .signInWithEmailAndPassword(this.email, this.password)
+        .then(() => {
+          alert('ログインが完了しました')
+          this.$router.push('/')
         })
         .catch((error) => {
           switch (error.code) {
             case 'auth/invalid-email':
               alert('メールアドレスの形式が違います。')
               break
-            case 'auth/email-already-in-use':
-              alert('このメールアドレスはすでに使われています。')
+            case 'auth/user-disabled':
+              alert('ユーザーが無効になっています。')
               break
-            case 'auth/weak-password':
-              alert('パスワードは6文字以上で入力してください。')
+            case 'auth/user-not-found':
+              alert('ユーザーが存在しません。')
+              break
+            case 'auth/wrong-password':
+              alert('パスワードが間違っております。')
               break
             default:
               alert('エラーが起きました。しばらくしてから再度お試しください。')
---
+++ plugins/firebase.js
@@ -0,0 +1,11 @@
+
+ // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDWB-OJ2E4Giw_Yzs9dYclO1PZLvR7XyFA",
+  authDomain: "takimoto-bca63.firebaseapp.com",
+  projectId: "takimoto-bca63",
+  storageBucket: "takimoto-bca63.appspot.com",
+  messagingSenderId: "1096261840839",
+  appId: "1:1096261840839:web:f441b6193329d5545b33e2",
+  measurementId: "G-D463RL5Y86"
+};
